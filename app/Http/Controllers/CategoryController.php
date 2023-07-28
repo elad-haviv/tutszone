5520ba6bfb5071ae12317949a4537b82ad05d228
Elad Bernard Haviv <elad.haviv@gmail.com>
2023-07-28 18:06:26 +0300

    added category routes with policy tests

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(Request $r) {
+    public function index(Request $r)
+    {
         $categories = Category::whereNull("parent_id")->get();
         $res = "List of Categories: <br />";
         foreach ($categories as $category) {
-            $res .= "<a href='". route('category', ['category' => $category]) ."'>{$category->title}</a> <br />";
+            $res .= "<a href='" . route('category.show', ['category' => $category]) . "'>{$category->title}</a> <br />";
         }
         return $res;
     }
 
-    public function show(Request $r, Category $category) {
+    public function show(Request $r, Category $category)
+    {
         return "Category: {$category->title}.";
+    }
+
+    public function create(Request $r)
+    {
+        throw new Exception("Unimplemented");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])
+        ->name('category.list')
+        ->can('viewAny', [Category::class]);
+    Route::get('/{category:slug}', [CategoryController::class, 'show'])
+        ->name('category.show')
+        ->middleware('can:view,category');
+
+    Route::middleware('can:create,category')->group(function () {
+        Route::get('/create', [CategoryController::class, 'category.new']);
+        Route::post('/create', [CategoryController::class, 'category.create']);
+    });
+
+    Route::middleware('can:update,category')->group(function () {
+        Route::get('/edit/{category:slug}', [CategoryController::class, 'category.edit']);
+        Route::post('/edit/{category:slug}', [CategoryController::class, 'category.update']);
+    });
+
+    Route::middleware('can:delete,category')->group(function () {
+        Route::get('/delete/{category:slug}', [CategoryController::class, 'category.edit']);
+        Route::post('/edit/{category:slug}', [CategoryController::class, 'category.update']);
+    });
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
