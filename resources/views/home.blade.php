9ef7351189b0b916747d345c06c99d550519a4b7
Elad Bernard Haviv <elad.haviv@gmail.com>
2023-07-28 07:31:06 +0300

    added language phrases

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
-@extends("layout.normal")
+@extends('layout.normal')
 
-@section("title")
+@section('title')
     @parent - Home
 @endsection
 
-@section("content")
+@section('content')
 
     @if (Auth::check())
         <section class="gradient-1">
             User Hero Section
         </section>
         <section>
-            <h2>{{ __("continue-learning") }}</h2>
+            <h2>{{ __('continue-learning') }}</h2>
             <ul>
                 @foreach (App\Models\Course::getLastEnrolled() as $enrollment)
                     <x-course-card :course="$enrollment->course" />
@@ -19,23 +19,25 @@
             </ul>
         </section>
         <section>
-            <h2>{{ __("recommended-courses") }}</h2>
+            <h2>{{ __('recommended-courses') }}</h2>
             <x-course-list :courses="App\Models\Course::getRecommended()" />
         </section>
-        @else
+    @else
         <section class="gradient-1 uk-text-center uk-padding-large">
-            <h1 class="uk-heading-large uk-padding-large">{{ __("hero.title", ["name" => config("app.name")]) }}</h1>
-            <p class="uk-text-lead">{{ __("hero.text") }}</p>
+            <h1 class="uk-heading-large uk-padding-large">{{ __('hero.title', ['name' => config('app.name')]) }}</h1>
+            <p class="uk-text-lead">{{ __('hero.text') }}</p>
         </section>
     @endif
     <section>
-        <h2>{{ __("bestseller-courses") }}</h2>
-        @foreach (App\Models\Course::getBestsellers() as $enrollment)
-            <x-course-card :course="$enrollment->course()->first()" />
-        @endforeach
+        <h2>{{ __('bestseller-courses') }}</h2>
+        <div>
+            @foreach (App\Models\Course::getBestsellers() as $enrollment)
+                <x-course-card :course="$enrollment->course()->first()" />
+            @endforeach
+        </div>
     </section>
     <section>
-        <h2>{{ __("categories") }}</h2>
+        <h2>{{ __('categories') }}</h2>
         <x-category-list :categories="App\Models\Category::getTopLevel()" />
     </section>
 @endsection
