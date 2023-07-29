e463aabb9234e9d90cc963a77cd59732ceb910d3
Elad Bernard Haviv <elad.haviv@gmail.com>
2023-07-29 10:24:54 +0300

    added js depenndency loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,21 @@
 import './bootstrap';
-import UIkit from 'uikit';
-
 import Alpine from 'alpinejs';
-import $ from 'jquery';
-import UIkit from 'uikit';
 import MathJax from 'mathjax'
+import UIkit from 'uikit';
+import Prism from 'prismjs';
+import jQuery from 'jquery';
 
+window.$ = jQuery;
 window.Alpine = Alpine;
-
 Alpine.start();
+
+window.UIkit = UIkit;
+
+window.Prism = Prism;
+
+MathJax.init({
+    loader: { load: ['input/tex', 'output/svg'] }
+}).then((MathJax) => {
+    const svg = MathJax.tex2svg('\\frac{1}{x^2-1}', { display: true });
+    console.log(MathJax.startup.adaptor.outerHTML(svg));
+}).catch((err) => console.log(err.message));
